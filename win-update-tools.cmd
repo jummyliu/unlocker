5c1eaacb27866161767a3132760df0a23d340a25
jummyliu <jummyliu@foxmail.com>
2020-03-24 15:37:45 +0800

    [+] delete .exe

--- win-update-tools.cmd
+++ win-update-tools.cmd
@@ -18,7 +18,8 @@ for /F "tokens=2* delims=	 " %%A in ('REG QUERY %KeyName% /v InstallPath') do se
 echo VMware is installed at: %InstallPath%
 
 echo Getting VMware Tools...
-gettools.exe
+py gettools.py --target=11.1.0
+REM gettools.exe
 xcopy /F /Y .\tools\darwin*.* "%InstallPath%"
 
 popd
